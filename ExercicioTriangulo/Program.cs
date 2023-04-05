5cdc13fda08a3b51f53930ea9f91bc07fdf97c89
Alisson Branco Scopel <alli_scopel@hotmail.com>
2023-04-05 18:39:40 -0300

    Add project files.

---
+++ ExercicioTriangulo/Program.cs
@@ -0,0 +1,66 @@
+﻿
+namespace ExercicioTriangulo
+{
+    internal class Program
+    {      
+        static void ObterValoresDosLados(Triangulo triangulo)
+        {
+            Console.Write("Digite o primeiro Lado: ");
+            triangulo.lado1 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Digite o segundo Lado: ");
+            triangulo.lado2 = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Digite o terceiro Lado: ");
+            triangulo.lado3 = int.Parse(Console.ReadLine());
+        }
+        
+        static void MensagemDaResposta(Triangulo triangulo)
+        
+        {
+
+            if (triangulo.VerificarSeehUmTriangulo())
+            {
+
+                if (triangulo.equilatero)
+                {
+                    Console.WriteLine("É um triângulo Equilátero");
+                }
+                else if ( triangulo.escaleno) 
+                {
+                    Console.WriteLine("É um triângulo Escaleno");
+                }
+                else if ( triangulo.isoceles)
+                {
+                    Console.WriteLine(" É um triângulo Isósceles ");
+                }
+                else
+                {
+                    Console.WriteLine("Não é um triângulo");
+                }
+
+            }
+
+
+            
+
+
+            
+
+
+
+
+
+        }
+
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo();
+            ObterValoresDosLados(triangulo);
+            triangulo.VerificarSeehUmTriangulo();
+            Console.WriteLine();
+            triangulo.VerTipoDoTriangulo();
+            MensagemDaResposta(triangulo);
+        }
+    }
+}
---
+++ ExercicioTriangulo/Triangulo.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioTriangulo
+{
+    internal class Triangulo
+    {
+        
+
+
+        public int lado1;
+        public int lado2;
+        public int lado3;
+        public bool ehtriangulo = true;
+        public bool isoceles, equilatero, escaleno;
+
+
+        public bool VerificarSeehUmTriangulo()
+        {
+
+            if (lado1 > lado2 + lado3 || lado2 > lado1 + lado3 || lado3 > lado1 + lado2)
+            {
+                return false;
+                
+            }
+            else if (lado1 < lado2 + lado3 || lado2 < lado1 + lado3 || lado3 < lado1 + lado2)
+            {
+                              
+                return true;
+
+            }
+
+            return ehtriangulo;
+        }
+
+        public bool VerTipoDoTriangulo()
+        {
+
+            if (VerificarSeehUmTriangulo())
+            {
+
+                if (lado1 == lado2 && lado3 == lado1 && lado2 == lado3)
+                {
+                    equilatero = true;
+                    return equilatero;                  
+                }
+
+                else if (lado1 != lado2 && lado2 != lado3)
+                {
+                    isoceles = true;
+                    return isoceles;
+                }
+
+                else if (lado1 == lado2 && lado2 != lado3 || lado2 == lado3 && lado2 != lado1 || lado1 == lado3 && lado3 != lado2)
+                {
+                    escaleno = true;
+                    return escaleno;
+
+                }
+            }
+            return false;
+        }
+      
+    }    
+
+}
+
+
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
